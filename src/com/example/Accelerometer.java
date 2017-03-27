f102536cb7b80eb45a4a278f86a57a6f8428b028
Frank E Weiss <feweiss@gmail.com>
2017-03-26 20:25:15 -0700

    Refactor: clean up the recurrence filter code

--- src/com/example/Accelerometer.java
+++ src/com/example/Accelerometer.java
@@ -178,23 +178,25 @@ implements SensorEventListener {
     */
     class Filter
     extends TimerTask {
-        private float a0 = (float) +0.535144118;
-        private float a1 = (float) +0.132788237;
-        private float a2 = (float) -0.402355882;
-        private float b1 = (float) -0.154508496;
-        private float b2 = (float) -0.0625;
-        private float x_1, x_2, y_1, y_2;
+        private final float a0 = (float) +0.535144118;
+        private final float a1 = (float) +0.132788237;
+        private final float a2 = (float) -0.402355882;
+        private final float b1 = (float) -0.154508496;
+        private final float b2 = (float) -0.062500000;
+
+        private float x0, x1, x2;
+        private float y0, y1, y2;
 
         @Override
         public void run() {
-            float x_0 = currentSample;
-            float y_0 = a0 * x_0 + a1 * x_1 + a2 * x_2
-                    + b1 * y_1 + b2 * y_2;
-            currentFilter = y_0;
-            x_2 = x_1;
-            x_1 = x_0;
-            y_2 = y_1;
-            y_1 = y_0;
+            x0 = currentSample;
+            y0 = a0 * x0 + a1 * x1 + a2 * x2
+               + b1 * y1 + b2 * y2;
+            currentFilter = y0;
+            x2 = x1;
+            x1 = x0;
+            y2 = y1;
+            y1 = y0;
         }
     }
 }
